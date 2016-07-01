ae30293e543d367ca8f26051d7608441a66d33ff
Jeffrey Chen <jeff@mechazawa.debian>
2016-06-30 19:28:44 -0700

    initial commit

---
+++ .vimrc
@@ -0,0 +1,29 @@
+execute pathogen#infect()
+set t_Co=256
+syntax on
+filetype plugin indent on
+inoremap jk <esc>
+map ; :
+set backspace=indent,eol,start
+set clipboard=unnamedplus
+set number
+set history=700
+set shiftwidth=2
+set tabstop=2
+set ai
+set si
+set wrap
+map <C-n> :NERDTreeToggle<CR>
+let g:user_emmet_leader_key='<C-E>'
+set laststatus=2
+let g:airline_powerline_fonts=1
+let g:airline_theme='bubblegum'
+"colorscheme koehler
+colorscheme evolution
+let g:JSHintHighlightErrorLine = 0
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+set splitbelow
+set splitright
---
+++ autostart.sh
@@ -0,0 +1,38 @@
+#autostart script. remember & after each command
+
+#exec xrandr --output DP-0 --primary --left-of DVI-I-1 &
+
+#set wallpaper
+#exec feh --bg-scale /home/jeff/Pictures/walls/crab2.png &
+#exec feh --bg-scale /home/jeff/Pictures/walls/unnamed2.png &
+#exec feh --bg-fill /home/jeff/Pictures/walls/rain.jpg &
+#exec feh --bg-fill ~/Pictures/react/1451518855892.png &
+#exec feh --bg-fill ~/Pictures/react/1451522381053.png &
+#exec feh --bg-fill /home/jeff/Pictures/kpop/spica.jpg &
+#exec feh --bg-scale /home/jeff/Pictures/walls/scandal.jpg &
+#exec feh --bg-scale /home/jeff/Pictures/walls/babymetal.jpg &
+#exec feh --bg-scale /home/jeff/Pictures/walls/yotsuba.jpg &
+#exec feh --bg-scale /home/jeff/Pictures/walls/pixel_waterfall.png &
+#exec feh --bg-scale /home/jeff/Pictures/walls/garden_of_words_day.PNG &
+#exec feh --bg-fill /home/jeff/Pictures/walls/ushijima.jpg &
+#exec feh --bg-fill /home/jeff/Pictures/walls/dankness3.jpg &
+#exec feh --bg-scale /home/jeff/Pictures/sexy.png &
+#exec feh --bg-fill /home/jeff/Pictures/old_walls/walls/oceancloud.jpg
+#exec feh --bg-fill /home/jeff/Pictures/walls/babymetal2.jpg &
+#exec feh --bg-fill /home/jeff/Pictures/walls/1440/temple.jpg &
+exec feh --bg-fill /home/jeff/Pictures/walls/1440/kpop7.jpg &
+
+#start flux
+exec xflux -z 95116 &
+
+#disable bell sound
+exec xset -b &
+
+#start tint2 taskbar
+exec tint2 &
+
+#start steam
+exec steam &
+
+#disable screen saver
+exec xset s off &
